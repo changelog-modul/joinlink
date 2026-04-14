231dae0eccdd2dae5bea3eb9ba1a000aad74d6d1
PHOENIXXx-Code <57739034+changelog-modul@users.noreply.github.com>
2026-04-14 19:23:02 +0700

    Update join.js

--- api/join.js
+++ api/join.js
@@ -2,32 +2,10 @@ export default function handler(req, res) {
   const { placeId, jobId } = req.query;
 
   if (!placeId || !jobId) {
-    return res.status(400).send("Missing placeId or jobId");
+    return res.status(400).send("Missing params");
   }
 
-  const deepLink = `roblox://placeId=${placeId}&gameInstanceId=${jobId}`;
-  const webFallback = `https://www.roblox.com/games/${placeId}`;
+  const url = `https://www.roblox.com/games/start?placeId=${placeId}&gameInstanceId=${jobId}`;
 
-  res.setHeader("Content-Type", "text/html");
-  res.send(`
-    <html>
-      <head>
-        <title>Joining Server...</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      </head>
-      <body style="background:#0f172a;color:white;text-align:center;padding-top:50%;">
-        <h2>Joining server...</h2>
-
-        <script>
-          // coba buka Roblox langsung
-          window.location.href = "${deepLink}";
-
-          // fallback kalau gagal
-          setTimeout(function(){
-            window.location.href = "${webFallback}";
-          }, 2000);
-        </script>
-      </body>
-    </html>
-  `);
+  return res.redirect(302, url);
 }
