7b7e7715ad87b1c7104ec8ade998b2bd587e18bd
PHOENIXXx-Code <57739034+changelog-modul@users.noreply.github.com>
2026-04-14 19:06:16 +0700

    Update join.js

--- api/join.js
+++ api/join.js
@@ -1,11 +1,11 @@
 export default function handler(req, res) {
   const { placeId, jobId } = req.query;
 
-  if (!placeId || !jobId) {
-    return res.status(400).send("Missing params");
-  }
+  const deepLink = `roblox://placeId=${placeId}&gameInstanceId=${jobId}`;
 
-  const url = `https://www.roblox.com/games/start?placeId=${placeId}&gameInstanceId=${jobId}`;
+  res.writeHead(302, {
+    Location: deepLink,
+  });
 
-  return res.redirect(302, url);
+  res.end();
 }
